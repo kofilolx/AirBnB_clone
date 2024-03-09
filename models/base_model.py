dfb51a179ab7872e7e57addef803a287c8e20e9e
kofilolx <kofyamando@gmail.com>
2024-03-09 00:06:34 -0500

    Project : AirBnB_wickCreator

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,31 @@ from uuid import uuid4
 
 
 tFormat = "%Y-%m-%dT%H:%M:%S.%f"
-coreName = ""
-newDict = ""
+# cl_Name = ""
+#new_Dict = ""
 
 
 class BaseModel:
     """The BaseModel serves as the foundation from which future classes will inherit."""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of BaseModel class"""
+        """Initialization of BaseModel class
+
+        Args:
+            *args: Unused.
+            **kwargs (dict): key/value pairs of attr
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, tFormat)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
 
     def save(self):
@@ -26,7 +42,8 @@ class BaseModel:
 
     def __str__(self):
         """Return the string representation of the BaseModel instance for printing."""
-
+        cl_Name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cl_Name, self.id, self.__dict__)
 
     def to_dict(self):
         """Return a dictionary representing the BaseModel instance.
@@ -34,6 +51,11 @@ class BaseModel:
         Including the key/value pair "__class__"
         indicating the object's class name.
         """
+        new_Dict = self.__dict__.copy()
+        new_Dict["created_at"] = self.created_at.isoformat()
+        new_Dict["updated_at"] = self.updated_at.isoformat()
+        new_Dict["__class__"] = self.__class__.__name__
+        return new_Dict
 
 
 
